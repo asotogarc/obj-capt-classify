a3b8551b1a18e8458c0ab57ff41f1ed974e31088
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-10 00:43:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.write('hi')
+from streamlit_webrtc import webrtc_streamer
+import av
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+st.title("Aplicación Streamlit con Webcam")
+st.write("Haga clic en 'Start' para activar la webcam.")
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
