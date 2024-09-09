6be65ece6afed5531c3e75a2a1b80f7ccbefde07
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-10 00:18:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-# Ocultar el footer de Streamlit
-hide_streamlit_style = """
+hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Tu contenido de la aplicación
 st.write('hi')
